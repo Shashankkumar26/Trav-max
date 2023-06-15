4375dc170066d707bdc073d78f80d476c6a11856
sourabhvatscoder <sourabhvats96@gmail.com>
2023-06-15 11:04:23 +0530

    test

--- app/Views/plans.php
+++ app/Views/plans.php
@@ -4,7 +4,7 @@
         cursor: pointer;
     }
 </style>
-<div class="row g-0" style="display:flex; justify-content:center; align-items:center;">
+<div class="row container g-0" style="display:flex; justify-content:center; align-items:center;">
     <div class="col">
         <img class="img-fluid" src="/images/features.jpg" alt="">
     </div>
